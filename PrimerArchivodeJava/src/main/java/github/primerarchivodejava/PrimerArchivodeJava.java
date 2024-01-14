8b68cf3dfdbb1926ee8066f18693e8e7630b6366
Astivero-x3d <ismaelast2005@gmail.com>
2024-01-14 21:28:23 +0100

    Haciendo la practica, editado primera vez

---
+++ PrimerArchivodeJava/src/main/java/github/primerarchivodejava/PrimerArchivodeJava.java
@@ -0,0 +1,20 @@
+/*
+ * Esto es una prueba
+ */
+
+package github.primerarchivodejava;
+
+/**
+ *
+ * @author Ismael_Ast
+ */
+public class PrimerArchivodeJava {
+
+    public static void main(String[] args) {
+        System.out.println("Probando");
+        
+        for (int i=0; i < 10; i++){
+            System.out.println("HOLA");
+        }
+    }
+}
